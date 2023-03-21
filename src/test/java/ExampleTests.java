18a44a1f3db81dc2741e9f74290f5fe28a316d1b
Nika Zhordania <nika.zhordania.1@btu.edu.ge>
2023-03-21 23:50:39 +0400

    update: firstTest

--- src/test/java/ExampleTests.java
+++ src/test/java/ExampleTests.java
@@ -3,6 +3,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -11,15 +13,32 @@ import java.util.List;
 public class ExampleTests {
 
     @Test
-    public void firstTest(){
+    public void firstTest() {
         WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver = new ChromeDriver(options);
         driver.get("http://the-internet.herokuapp.com/");
         driver.manage().window().maximize();
-        WebElement button=driver.findElement(By.xpath("//ul/li/a"));
+        WebElement button = driver.findElement(By.xpath("//ul/li/a"));
         button.click();
         WebElement text = driver.findElement(By.xpath("//*[@id=\"content\"]/div/h3"));
-        Assert.assertEquals(text.getText(),"No Test");
+
+        Assert.assertEquals(text.getText(), "A/B Test Variation 1");
+
+        driver.navigate().back();
+
+        WebElement dropdownElement = driver.findElement(By.cssSelector("li a[href='/dropdown']"));
+        dropdownElement.click();
+
+        WebElement dropdownMenu = driver.findElement(By.id("dropdown"));
+
+        Select select = new Select(dropdownMenu);
+        select.selectByVisibleText("Option 1");
+
+        Assert.assertEquals(select.getFirstSelectedOption().getText(), "Option 1");
+
+        driver.close();
     }
     @Test
     public void locatorTest(){
